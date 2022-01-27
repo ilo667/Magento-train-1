3fce02e102afc235d0fcab75aec5c6ba27cf521c
Dianaeva <diana.yevstignyeyeva@gmail.com>
2022-01-27 14:00:37 +0200

    MAG-30: Send data from PHTML to JS. Create XML block. Create require JS module. Initialize and send data to the JS component from the template.

---
+++ app/design/frontend/Diana/default-theme/web/js/js-mag-30.js
@@ -0,0 +1,9 @@
+define([], function() {
+    'use strict';
+
+    return function(config, element) {
+        var data = config.dataFromPhtml;
+        console.log('mag-30');
+        console.log(data);
+    }
+});
