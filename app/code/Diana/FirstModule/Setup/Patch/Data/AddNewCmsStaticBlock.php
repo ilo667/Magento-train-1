f71616ed3d6e91e343c65162c0f9ab779d534137
Dianaeva <diana.yevstignyeyeva@gmail.com>
2022-01-31 18:57:39 +0200

    MAG-37: Create CMS block with setup patch. Create new CMS block through setup patch.

---
+++ app/code/Diana/FirstModule/Setup/Patch/Data/AddNewCmsStaticBlock.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Diana\FirstModule\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Cms\Model\BlockFactory;
+/**
+ * Class AddNewCmsStaticBlock
+ * @package Diana\FirstModule\Setup\Patch\Data
+ */
+class AddNewCmsStaticBlock implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var BlockFactory
+     */
+    private $blockFactory;
+
+    /**
+     * AddAccessViolationPageAndAssignB2CCustomers constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param PageFactory $blockFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        BlockFactory $blockFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->blockFactory = $blockFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $newCmsStaticBlock = [
+            'title' => 'CMS Block with Setup Patch',
+            'identifier' => 'cms-block-setup-patch',
+            'content' => '<div class="cms-terms">CMS Block with Setup Patch</div>',
+            'is_active' => 1,
+            'stores' => \Magento\Store\Model\Store::DEFAULT_STORE_ID
+        ];
+
+        $this->moduleDataSetup->startSetup();
+
+        /** @var \Magento\Cms\Model\Block $block */
+        $block = $this->blockFactory->create();
+        $block->setData($newCmsStaticBlock)->save();
+
+        $this->moduleDataSetup->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getVersion()
+    {
+        return '2.0.0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
