e456240aecdc73a27f18545f4b17ecba249a8572
Dianaeva <diana.yevstignyeyeva@gmail.com>
2022-02-04 19:06:30 +0200

    MAG-56: Checkout costomisations. Add the button edit cart on the aside panel, add two required fields in the registration form on the shipping step, add total information about order in the aside panel in the shipping step.

---
+++ app/design/frontend/Diana/default-theme/Magento_Checkout/web/js/view/summary/edit-cart-button.js
@@ -0,0 +1,18 @@
+define([
+    'uiComponent',
+    'ko'
+], function(Component, ko) {
+    'use strict';
+
+    return Component.extend({
+
+        defaults: {
+            template: 'Magento_Checkout/summary/edit-cart-button'
+        },
+        cartUrl: window.checkoutConfig.cartUrl,
+
+        initialize: function () {
+            this._super();
+        }
+    });
+});
