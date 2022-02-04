cfd9c158f3a9e5b6f655cb872504a59dcafdb73c
Dianaeva <diana.yevstignyeyeva@gmail.com>
2022-02-04 17:06:27 +0200

    MAG-55: Accordion products on cart page. Remove redundant tabs.

--- app/design/frontend/Diana/default-theme/Magento_Checkout/web/js/cart/checkout-cart-accordion.js
+++ app/design/frontend/Diana/default-theme/Magento_Checkout/web/js/cart/checkout-cart-accordion.js
@@ -7,26 +7,26 @@ define([
 ], function ($, mediaCheck, _) {
     'use strict';
 
-        mediaCheck({
-            media: '(min-width: 768px)',
+    mediaCheck({
+        media: '(min-width: 768px)',
 
-            /**
-             * Switch to Desktop Version.
-             */
-            entry: function() {
-                var checkoutCartAccordion = $('#checkout-cart-accordion');
-                if (!_.isUndefined(checkoutCartAccordion.data().mageAccordion)) {
-                    checkoutCartAccordion.accordion('destroy');
-                }
-            },
-
-            /**
-             * Switch to Mobile Version.
-             */
-            exit: function () {
-                $('#checkout-cart-accordion').accordion({
-                    "collapsible": true
-                });
+        /**
+         * Switch to Desktop Version.
+         */
+        entry: function() {
+            var checkoutCartAccordion = $('#checkout-cart-accordion');
+            if (!_.isUndefined(checkoutCartAccordion.data().mageAccordion)) {
+                checkoutCartAccordion.accordion('destroy');
             }
-        });
+        },
+
+        /**
+         * Switch to Mobile Version.
+         */
+        exit: function () {
+            $('#checkout-cart-accordion').accordion({
+                "collapsible": true
+            });
+        }
+    });
 });
