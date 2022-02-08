8f1a342f9d5d10037b3c48450539953499e35597
Dianaeva <diana.yevstignyeyeva@gmail.com>
2022-02-08 18:51:21 +0200

    MAG-58 - Login page. On the login page there's only one input for entering a user's email. After clicking the "Check Email" button the field for entering password appears if the email exists in a database. If the email doesn't registered, the user is offered to create an account.

---
+++ app/design/frontend/Diana/default-theme/web/js/check-email-exists.js
@@ -0,0 +1,56 @@
+define([
+    'uiComponent',
+    'ko',
+    'jquery',
+    'mage/url',
+    'underscore',
+    'mage/translate'
+], function(Component, ko, $, url, _, $t) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            isPasswordVisible: ko.observable(false),
+            isCheckEmailVisible: ko.observable(true),
+            isCreateAccountVisible: ko.observable(false),
+            userEmail: ko.observable(),
+            checkMessage: ko.observable()
+        },
+        initialize: function() {
+            this._super();
+        },
+        checkEmail : function() {
+            var self = this;
+            var userEmail = this.userEmail();
+            var query = `{
+                    isEmailAvailable(email: "${userEmail}" ) {
+                        is_email_available
+                    }}`;
+            $.ajax({
+                method: "POST",
+                url: url.build("graphql"),
+                data: JSON.stringify({"query": query}),
+                contentType: 'application/json',
+                success: function(result) {
+                    if (!_.isNull(result.data.isEmailAvailable)) {
+                        if(!result.data.isEmailAvailable.is_email_available) {
+                            self.checkMessage($t("This email exists. Please, enter the password."));
+                            self.isPasswordVisible(true);
+                            self.isCheckEmailVisible(false);
+                            self.isCreateAccountVisible(false);
+                        } else {
+                            self.checkMessage($t("You can create an account."));
+                            self.isCreateAccountVisible(true);
+                        }
+                    } else {
+                        self.checkMessage($t("Please, enter the correct email. E.g. 'john@example.com'."));
+                        self.isCreateAccountVisible(false);
+                    }
+                },
+                error: function(result) {
+                    console.log(result);
+                }
+            });
+        }
+    });
+});
